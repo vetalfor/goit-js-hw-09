be37883848ff9f34ba1efe178745faa5414bb30c
vetalfor <129608172+vetalfor@users.noreply.github.com>
2023-10-18 17:01:56 +0000

    Deploying to gh-pages from @ vetalfor/goit-js-hw-09@62147895851e0d115d5816a8a5540235b4308682 🚀

---
+++ 01-color-switcher.7a10aa54.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;document.addEventListener("DOMContentLoaded",(()=>{e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.7a10aa54.js.map
